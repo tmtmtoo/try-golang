a779bc3d8d2cf4a49852f5454dbeb8881b7ac2e2
tmtmtoo <tmtmt2209@gmail.com>
2025-09-27 17:20:42 +0900

    chore: force change migrations

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,26 +1,32 @@
 create table foo (
-    id serial primary key,
+    id uuid primary key,
     value text not null
 );
 
 create table bar (
-    id serial primary key,
+    id uuid primary key,
     value text not null,
-    foo_id serial not null,
+    foo_id uuid not null,
 
     constraint fk_foo foreign key (foo_id) references foo(id)
 );
 
+create index idx_bar_foo_id on bar(foo_id);
+
 create table baz (
-    id serial primary key,
+    id uuid primary key,
     value text not null
 );
 
 create table foo_baz (
-    foo_id serial not null,
-    baz_id serial not null,
+    foo_id uuid not null,
+    baz_id uuid not null,
 
     constraint fk_foo foreign key (foo_id) references foo(id),
     constraint fk_baz foreign key (baz_id) references baz(id),
     primary key (foo_id, baz_id)
 );
+
+create index idx_foo_baz_foo_id on foo_baz(foo_id);
+
+create index idx_foo_baz_baz_id on foo_baz(baz_id);
